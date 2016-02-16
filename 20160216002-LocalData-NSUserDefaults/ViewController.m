ceef7dd08082b5b46b7b1ebe3c9701d7f36d3fd7
raineryue <jinlei_yue@sina.com>
2016-02-16 16:01:16 +0800

    NSUserDefaults存储数据
    NSUserDefaults存储数据

--- 20160216002-LocalData-NSUserDefaults/ViewController.m
+++ 20160216002-LocalData-NSUserDefaults/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+// 创建一个NSUserDefaults对象:在沙盒中的位置：/Library/Preferences
+#define kUserDefault [NSUserDefaults standardUserDefaults]
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+/**
+ *  存储按钮点击事件
+ */
+- (IBAction)saveClickAction:(id)sender {
+    NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+
+    NSLog(@"%@", documentsPath);
+    
+    // 通过键值的方法将数据添加到NSUserDefaults对象中
+    [kUserDefault setObject:@"Rainer" forKey:@"LoginName"];
+    [kUserDefault setObject:@"123456" forKey:@"Password"];
+    
+    // 同步NSUserDefaults对象到沙盒中
+    [kUserDefault synchronize];
+}
+
+/**
+ *  读取按钮点击事件
+ */
+- (IBAction)readClickAction:(id)sender {
+    // 通过键值的方法从NSUserDefaults对象中读取数据
+    NSString *loginName = [kUserDefault objectForKey:@"LoginName"];
+    NSString *password = [kUserDefault objectForKey:@"Password"];
+    
+    NSLog(@"LoginName:%@, Password:%@", loginName, password);
 }
 
 - (void)didReceiveMemoryWarning {
